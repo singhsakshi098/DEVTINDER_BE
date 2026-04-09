aa21b27498e03bbb245451de27bb7fcae5cefe96
sakshi <sakshi0981456@gmail.com>
2026-04-09 19:27:42 +0530

    exploring routing

--- src/app.js
+++ src/app.js
@@ -23,6 +23,10 @@ app.use("/hello/2", (req, res) => {
     res.send("aabra ka dabra");
 });
 
+app.use("/hyee", (req, res) => {
+    res.send("aabra ka dabra and hye");
+});
+
 app.use("/hello", (req, res) => {
     res.send("Hello from the server!");
 });
