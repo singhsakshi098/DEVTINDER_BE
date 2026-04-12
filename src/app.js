309c8498c918eadd6731498e93741a3d56a279e5
sakshi <sakshi0981456@gmail.com>
2026-04-12 22:04:15 +0530

    readme updation

--- src/app.js
+++ src/app.js
@@ -23,9 +23,9 @@ app.use("/hello/2", (req, res) => {
     res.send("aabra ka dabra");
 });
 
-// app.use("/hyee", (req, res) => {
-//     res.send("aabra ka dabra and hye");
-// });
+app.use("/hyee", (req, res) => {
+    res.send("aabra ka dabra and hye");
+});
 
 app.use("/hello", (req, res) => {
     res.send("Hello from the server!");
